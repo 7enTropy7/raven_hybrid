883bf5a318723b5218a87b59176dad3fb23ca918
Anirudh Menon <senseianirudh28@gmail.com>
2022-05-31 15:43:53 +0530

    auth for requests decorator completed

--- ravsock/handlers/auth_decorator.py
+++ ravsock/handlers/auth_decorator.py
@@ -1,13 +1,16 @@
 import functools
 from ..globals import globals as g
 import sys
+from aiohttp import web
 
 def authenticate_token(method):
     @functools.wraps(method)
     def wrapper(request, *args, **kwargs):
         if request.headers["Authorization"] == "<ravenverse_token>":
             print("\n======= Authentication Successful: {} =======".format(method.__name__)) # where does user come from?!
-        return method(request, *args, **kwargs)
+            return method(request, *args, **kwargs)
+        else:
+            return web.json_response({"Error": "Error Authenticating Token"}, content_type="application/json", status=400)
     return wrapper
 
 def socketio_authenticate_token(method):
